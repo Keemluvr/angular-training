e5dffee77057f7fd0ec8c9a86aeb2ed1fe547aaf
Keila Fernandes <keiladaianefernandes24@gmail.com>
2019-03-19 11:05:07 -0300

    estrutura do projeto

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DiretivaNgifComponent
+    DiretivaNgifComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
